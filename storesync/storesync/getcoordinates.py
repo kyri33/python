29d4ebf576514970499306fea9c60b3ed642af58
Kyriakos Ioulianou <kyriakosioulianou@Kyriakoss-iMac.local>
2020-01-09 15:39:05 +0200

    awe

--- storesync/storesync/getcoordinates.py
+++ storesync/storesync/getcoordinates.py
@@ -8,14 +8,20 @@ def get_coordinates(inputAddress):
     
     inputAddress = inputAddress.replace('Cnr Conradie and Strand Streets' , 'Norkem Park High School')
     inputAddress = inputAddress.replace('Cnr Main & Kelly Roads' , 'Norkem Park High School')
-    g = geocoders.GoogleV3(api_key='AIzaSyDCwqJ1BMQx6tcNRliQwZFWhweQVK-vviQ')
-    location = g.geocode(inputAddress, timeout=10)
+    
+    try:
+        location = g.geocode(inputAddress, timeout=10)
+    except:
+        return {'latitude': 0, 'longitude': 0}
+
     coordinate = {'latitude': 0, 'longitude':0}
     
-    coordinate['latitude'] = location.latitude
-    coordinate['longitude'] = location.longitude
-
-    if location == "":
+    if location == "" or location == None:
         coordinate = {'latitude': 0, 'longitude':0}
     else:
-        return coordinate
+        coordinate['latitude'] = location.latitude
+        coordinate['longitude'] = location.longitude
+    
+    return coordinate
+
+    
